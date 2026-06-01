c5e87d181330f69d763a73851c01d76f48360178
jaykumar398607-hub <jaykumar398607@gmail.com>
2026-06-01 20:44:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import PortfolioWebsite from './PortfolioWebsite'
-
 function App() {
-  return <PortfolioWebsite />
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      minHeight: "100vh",
+      padding: "50px"
+    }}>
+      <h1>Jaykumar Khunt Portfolio</h1>
+      <p>If you can see this, React is working.</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
